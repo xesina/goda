6a15c4707ec3593b629d2ff0e5704e577126dce2
Sina Saeidi <xesina@gmail.com>
2019-05-12 02:13:00 +0430

    implement most known functionalities for a fixed size stack with table driven test

--- stack/stack.go
+++ stack/stack.go
@@ -1,32 +1,79 @@
-package main 
+package main
+
+import (
+	"errors"
+)
+
+const initialTop = -1
+
+var (
+	errStackEmpty = errors.New("stack: stack is empty")
+	errStackFull  = errors.New("stack: stack is full")
+)
 
 // Stack represents a stack of int elements
+// Notice: This isn't concurrent safe stack
 type Stack struct {
 	data []int
+	top  int
 }
 
 // NewStack creates and returns a new stack
-func NewStack(size int) *Stack {
+func NewStack(size uint) *Stack {
 	return &Stack{
 		data: make([]int, size),
+		top:  initialTop,
 	}
 }
 
-// Pop removes and returns the top element on stack 
-func (s *Stack) Pop() int  {
-	return 0
+// Pop removes and returns the top element on stack
+func (s *Stack) Pop() (int, error) {
+	if s.IsEmpty() {
+		return 0, errStackEmpty
+	}
+
+	item := s.data[s.top]
+	s.top--
+
+	return item, nil
+}
+
+// Peek returns the value at the top of the stack, but does not remove it.
+func (s *Stack) Peek() (int, error) {
+	if !s.IsEmpty() {
+		return s.data[s.top], nil
+	}
+
+	return 0, errStackEmpty
 }
 
 // Push add new element to top of stack
-func (s *Stack) Push(item int)  {
-	
+func (s *Stack) Push(item int) error {
+	if s.IsFull() {
+		return errStackFull
+	}
+
+	s.top++
+	s.data[s.top] = item
+
+	return nil
 }
 
 // Size returns the size of stack
 func (s *Stack) Size() int {
-	return 0
+	return len(s.data)
 }
 
-func main()  {
-	
+// IsEmpty returns true if stack is empty
+func (s *Stack) IsEmpty() bool {
+	return s.top == initialTop
+}
+
+// IsFull returns true if stack is full
+func (s *Stack) IsFull() bool {
+	return s.top == s.Size()-1
+}
+
+func main() {
+
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -3,48 +3,165 @@ package main
 import (
 	"testing"
 )
-func TestNewStack(t *testing.T)  {
-	size := 4
-	s := NewStack(size)
-	
-	if s.data == nil {
-		t.Errorf("s.data is nil, want not nil")
+
+func TestNewStack(t *testing.T) {
+	cases := []struct {
+		size uint
+	}{
+		{size: 4},
+		{size: 1},
+		{size: 2},
+		{size: 0},
+		{size: 10000},
 	}
 
-	if len(s.data) != size {
-		t.Errorf("len(s.data) = %d, want %d", len(s.data), size)
-	}	
+	for _, c := range cases {
+		s := NewStack(c.size)
+		if s.data == nil {
+			t.Errorf("s.data is nil, want not nil")
+		}
+
+		if len(s.data) != int(c.size) {
+			t.Errorf("len(s.data) = %d, want %d", len(s.data), int(c.size))
+		}
+	}
+
 }
 
-func TestSize(t *testing.T)  {
-	size := 4
-	s := NewStack(size)
-	
-	if s.Size() != size {
-		t.Errorf("s.Size() = %d, want %d", s.Size(), size)
-	}	
+func TestSize(t *testing.T) {
+	cases := []struct {
+		size uint
+	}{
+		{size: 4},
+		{size: 1},
+		{size: 2},
+		{size: 0},
+	}
+
+	for _, c := range cases {
+		s := NewStack(c.size)
+		if s.Size() != len(s.data) || s.Size() != int(c.size) {
+			t.Errorf("s.Size() = %d, want %d", s.Size(), int(c.size))
+		}
+	}
 }
 
-func TestPop(t *testing.T)  {
-	size := 4
-	item:= 1
-	s := NewStack(size)
-	s.Push(item)
+func TestPush(t *testing.T) {
+	cases := []struct {
+		stack []int // initial stack items
+		size  int   // size indicates the stack size
+		push  int   // new item that we're going to push
+		want  int   // peek item after push
+		err   error // push error
+	}{
+		{
+			stack: []int{},
+			size:  0,
+			push:  1,
+			want:  0,
+			err:   errStackFull,
+		},
+		{
+			stack: []int{1, 2, 3},
+			size:  6,
+			push:  4,
+			want:  4,
+			err:   nil,
+		},
+	}
 
-	p := s.Pop()
-	if p != item {
-		t.Errorf("s.Pop() = %d, want %d", p, item )
-	} 
+	for _, c := range cases {
+		s := loadToStack(c.size, c.stack)
+		err := s.Push(c.push)
+		if err != c.err {
+			t.Fatalf("err = \"%s\", want err = \"%s\"", err, c.err)
+		}
+		p, _ := s.Peek()
+		if p != c.want {
+			t.Fatalf("got %d, want %d", p, c.want)
+		}
+	}
+
 }
 
-func TestPush(t *testing.T)  {
-	size := 4
-	item := 1
-	s := NewStack(size)
+func TestPop(t *testing.T) {
+	cases := []struct {
+		stack []int // initial stack items
+		want  int   // item that should be returned and removed from stack
+		err   error // push error
+	}{
+		{
+			stack: []int{},
+			want:  0,
+			err:   errStackEmpty,
+		},
+		{
+			stack: []int{1, 2, 3},
+			want:  3,
+			err:   nil,
+		},
+		{
+			stack: []int{1},
+			want:  1,
+			err:   nil,
+		},
+	}
 
-	s.Push(item)
-	p := s.Pop()
-	if p != item {
-		t.Errorf("s.Push(%d), got s.Pop() = %d, want %d", item, p, item)
-	} 
+	for _, c := range cases {
+		s := loadToStack(len(c.stack), c.stack)
+		top := s.top
+		p, err := s.Pop()
+		if err != c.err {
+			t.Fatalf("err = \"%s\", want err = \"%s\"", err, c.err)
+		}
+
+		if p != c.want {
+			t.Fatalf("got %d, want %d", p, c.want)
+		}
+
+		if err != errStackEmpty && s.top != top-1 {
+			t.Fatalf("top = %d, want top = %d", s.top, top-1)
+		}
+	}
+
+}
+
+func TestPeek(t *testing.T) {
+	cases := []struct {
+		items []int
+		want  int
+		err   error
+	}{
+		{
+			items: []int{},
+			want:  0,
+			err:   errStackEmpty,
+		},
+		{
+			items: []int{1, 2, 3},
+			want:  3,
+			err:   nil,
+		},
+	}
+
+	for _, c := range cases {
+		s := loadToStack(len(c.items), c.items)
+		p, err := s.Peek()
+		if err != c.err {
+			t.Fatal(err)
+		}
+		if p != c.want {
+			t.Fatalf("got %d, want %d", p, c.want)
+		}
+
+	}
+}
+
+func loadToStack(size int, arr []int) *Stack {
+	s := NewStack(uint(size))
+	for _, v := range arr {
+		s.Push(v)
+	}
+
+	return s
 }
